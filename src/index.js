c52c5054aa620fce92779f6840eda7d785a5159d
Carlos Hernandez Topete <72361463+toshiroto@users.noreply.github.com>
2022-02-12 17:07:58 +0900

    First React APP Unordered List using JSX CODE

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
-//Create a react app from scratch.
-//It should display a h1 heading.
-//It should display an unordered list (bullet points).
-//It should contain 3 list elements.
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>The Best Dogs</h1>
+    <ul>
+      <li>Karlota</li>
+      <li>Jacinto</li>
+      <li>Olivia</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
