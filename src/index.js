143205e019ac15f293f2482001df2148f765dd5a
Carlos Hernandez Topete <72361463+toshiroto@users.noreply.github.com>
2022-02-14 14:55:56 +0900

    Attributes-Styling Atributes and styling JSX with CSS

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const img = "https://source.unsplash.com/random";
 ReactDOM.render(
   <div>
-    <h1>The Best Dogs</h1>
-    <ul>
-      <li>Karlota</li>
-      <li>Jacinto</li>
-      <li>Olivia</li>
-    </ul>
+    <h1 className="heading">The Best Dogs</h1>
+    <img alt"random src={img}></img>
   </div>,
   document.getElementById("root")
 );
